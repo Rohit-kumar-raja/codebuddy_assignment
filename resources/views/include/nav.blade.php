c31e06c921b9e08b9274df248858dafd5e7b3824
Raja <raja@raja.com>
2023-04-28 16:29:18 +0530

    all done

--- resources/views/include/nav.blade.php
+++ resources/views/include/nav.blade.php
@@ -51,10 +51,12 @@
                      aria-labelledby="userDropdown">
                
                      <div class="dropdown-divider"></div>
-                     <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                         <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                         Logout
-                     </a>
+               <form action="{{ route('logout')}}" method="post">
+                @csrf
+                <button class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                    Logout
+                </button></form>
                  </div>
              </li>
 
--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::resource('category', CategoryController::class);;
-Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::resource('category', CategoryController::class)->middleware('auth');
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
+require __DIR__ . '/auth.php';
